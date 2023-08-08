6a91208b5e13b5d8d9e2631c7abbe598e0bc3808
Pete Maan <pete.maan+github@gmail.com>
2023-08-09 00:45:21 +0100

    👷💚 DEV Update
    Adds appveyor build scripts

---
+++ build/install.ps1
@@ -0,0 +1,36 @@
+#---------------------------------#
+# Header                          #
+#---------------------------------#
+Write-Host "Installing Required Modules:" -ForegroundColor Yellow
+
+$RequiredModules = @(
+	"PowerShellGet"
+	"Pester",
+	"PSScriptAnalyzer",
+	"coveralls",
+	"PSCodeCovIo"
+)
+
+#---------------------------------#
+# Install NuGet                   #
+#---------------------------------#
+if(-not $IsCoreCLR) {
+	Write-Host "`tNuGet..."
+	$pkg = Install-PackageProvider -Name NuGet -Confirm:$false -Force -ErrorAction Stop
+	Write-Host "`t`tInstalled NuGet version '$($pkg.version)'"
+}
+#---------------------------------#
+# Install Required Modules        #
+#---------------------------------#
+foreach ($Module in $RequiredModules) {
+
+	Try {
+		Write-Host "`tInstalling: $Module..." -NoNewline
+		Install-Module -Name $Module -Repository PSGallery -Confirm:$false -Force -SkipPublisherCheck -ErrorAction Stop | Out-Null
+		Write-Host " OK" -ForegroundColor Green
+	}Catch {
+		Write-Host "Error" -ForegroundColor Red
+		throw $_
+	}
+
+}
---
+++ build/test.ps1
@@ -0,0 +1,63 @@
+#---------------------------------#
+# Header                          #
+#---------------------------------#
+Write-Host "Testing: PSVersion $($PSVersionTable.PSVersion)" -ForegroundColor Yellow
+$ManifestPath = Join-Path "$pwd" $(Join-Path "$env:APPVEYOR_PROJECT_NAME" "$env:APPVEYOR_PROJECT_NAME.psd1")
+Import-Module Pester -Force
+Import-Module $ManifestPath -Force
+#---------------------------------#
+# Run Pester Tests                #
+#---------------------------------#
+$files = Get-ChildItem $(Join-Path $ENV:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME) -Include *.psm1 -Recurse | Select-Object -ExpandProperty FullName
+
+# get default from static property
+$configuration = [PesterConfiguration]::Default
+# assing properties & discover via intellisense
+$configuration.Run.Path = '.\Tests'
+$configuration.Run.PassThru = $true
+$configuration.CodeCoverage.Enabled = $true
+$configuration.CodeCoverage.Path = $files
+$configuration.TestResult.Enabled = $true
+$configuration.TestResult.OutputFormat = 'NUnitXml'
+$configuration.TestResult.OutputPath = '.\TestsResults.xml'
+$configuration.Output.Verbosity = 'None'
+
+$result = Invoke-Pester -Configuration $configuration
+
+$res = $result | ConvertTo-Pester4Result
+
+Write-Host 'Uploading Test Results'
+$null = (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $(Resolve-Path .\TestsResults.xml))
+
+Remove-Item -Path $(Resolve-Path .\TestsResults.xml) -Force
+
+if ($env:APPVEYOR_REPO_COMMIT_AUTHOR -eq 'Pete Maan') {
+
+	#Write-Host 'Formating Code Coverage'
+	#$coverage = Format-Coverage -PesterResults $res -CoverallsApiToken $($env:coveralls_key) -BranchName $($env:APPVEYOR_REPO_BRANCH)
+
+	#$null = Export-CodeCovIoJson -CodeCoverage $res.CodeCoverage -RepoRoot $pwd -Path coverage.json
+
+	Write-Host 'Publishing Code Coverage'
+	#$null = Publish-Coverage -Coverage $coverage
+
+	$null = Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile codecov.sh
+
+	$null = bash codecov.sh -f coverage.xml
+
+	Remove-Item -Path $(Resolve-Path .\coverage.xml) -Force
+	Remove-Item -Path $(Resolve-Path .\codecov.sh) -Force
+
+}
+#---------------------------------#
+# Validate                        #
+#---------------------------------#
+if (($res.FailedCount -gt 0) -or ($res.PassedCount -eq 0)) {
+
+	throw "$($res.FailedCount) tests failed."
+
+} else {
+
+	Write-Host 'All tests passed' -ForegroundColor Green
+
+}
