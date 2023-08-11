4afcb5fe9a19befbaca78a7abbee91f3ee35a7de
Pete Maan <pete.maan+github@gmail.com>
2023-08-12 00:45:02 +0100

    Update install.ps1

--- build/install.ps1
+++ build/install.ps1
@@ -1,20 +1,20 @@
 #---------------------------------#
 # Header                          #
 #---------------------------------#
-Write-Host "Installing Required Modules:" -ForegroundColor Yellow
+Write-Host 'Installing Required Modules:' -ForegroundColor Yellow
 
 $RequiredModules = @(
-	"PowerShellGet"
-	"Pester",
-	"PSScriptAnalyzer",
-	"coveralls",
-	"PSCodeCovIo"
+	#"PowerShellGet"
+	'Pester',
+	'PSScriptAnalyzer',
+	'coveralls',
+	'PSCodeCovIo'
 )
 
 #---------------------------------#
 # Install NuGet                   #
 #---------------------------------#
-if(-not $IsCoreCLR) {
+if (-not $IsCoreCLR) {
 	Write-Host "`tNuGet..."
 	$pkg = Install-PackageProvider -Name NuGet -Confirm:$false -Force -ErrorAction Stop
 	Write-Host "`t`tInstalled NuGet version '$($pkg.version)'"
@@ -27,9 +27,9 @@ foreach ($Module in $RequiredModules) {
 	Try {
 		Write-Host "`tInstalling: $Module..." -NoNewline
 		Install-Module -Name $Module -Repository PSGallery -Confirm:$false -Force -SkipPublisherCheck -ErrorAction Stop | Out-Null
-		Write-Host " OK" -ForegroundColor Green
-	}Catch {
-		Write-Host "Error" -ForegroundColor Red
+		Write-Host ' OK' -ForegroundColor Green
+	} Catch {
+		Write-Host 'Error' -ForegroundColor Red
 		throw $_
 	}
 
